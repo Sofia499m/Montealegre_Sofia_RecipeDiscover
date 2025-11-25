26ea416dfeb4c78e00ce1db0552ce92fba747952
Sofia Montealegre <G00469283@atu.ie>
2025-11-25 20:40:04 +0000

    calling end point in API

--- Montealegre_Sofia_RecipeDiscover/MainPage.xaml.cs
+++ Montealegre_Sofia_RecipeDiscover/MainPage.xaml.cs
@@ -2,13 +2,18 @@
 {
     public partial class MainPage : ContentPage
     {
-        int count = 0;
+        private  readonly ServiceApi _serviceApi = new ServiceApi();
 
         public MainPage()
         {
             InitializeComponent();
         }
-
+        public async void OnSearchCompleted(object sender, EventArgs e)
+        {
+            string name = Search.Text;
+            var recipies = await _serviceApi.GetRecipesName(name);
+            FoodList.ItemsSource = recipies;
+        }
         
     }
 
---
+++ Montealegre_Sofia_RecipeDiscover/Meals.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Montealegre_Sofia_RecipeDiscover
+{
+	internal class Meals
+	{
+		public List<Recipes> meals { get; set; }
+	}
+}

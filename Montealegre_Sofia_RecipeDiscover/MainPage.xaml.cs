d048a02dc0cfccad723944d24c6b7b0c377bb0c6
Sofia Montealegre <G00469283@atu.ie>
2025-11-25 21:52:14 +0000

    Revert "calling end point in API"
    This reverts commit 26ea416dfeb4c78e00ce1db0552ce92fba747952.

--- Montealegre_Sofia_RecipeDiscover/MainPage.xaml.cs
+++ Montealegre_Sofia_RecipeDiscover/MainPage.xaml.cs
@@ -2,18 +2,13 @@
 {
     public partial class MainPage : ContentPage
     {
-        private  readonly ServiceApi _serviceApi = new ServiceApi();
+        int count = 0;
 
         public MainPage()
         {
             InitializeComponent();
         }
-        public async void OnSearchCompleted(object sender, EventArgs e)
-        {
-            string name = Search.Text;
-            var recipies = await _serviceApi.GetRecipesName(name);
-            FoodList.ItemsSource = recipies;
-        }
+
         
     }
 
